bbc9d8f2481bde5b27ee0afc931fe2184a120b06
unknown <omkar.patil@graymatrix.com>
2020-11-03 10:16:41 +0530

    Joins added

--- advance/run.py
+++ advance/run.py
@@ -29,14 +29,32 @@ from one_to_many import db, Pet, Person
 # db.session.commit()
 
 # ###### getting pet ocject list from owner object
-# some_owner = Person.query.filter_by(name='Omkar').first()
-# print(some_owner.name, "Owns following pets")
-#
-# for pet in some_owner.pets: ## .pets will give all the pets object in list
-#     print(pet.name)
+# # some_owner = Person.query.filter_by(name='Omkar').first()
+# # print(some_owner.name, "Owns following pets")
+# #
+# # for pet in some_owner.pets: ## .pets will give all the pets object in list
+# #     print(pet.name)
 
-##### fetching owner object from pet object
-some_pet = Pet.query.filter_by(name='Kalu').first()
-print(some_pet.name)
-print()
-print(some_pet.owner.name) ###some_pet.owner will return owner object
+# ##### fetching owner object from pet object
+# some_pet = Pet.query.filter_by(name='Kalu').first()
+# print(some_pet.name)
+# print()
+# print(some_pet.owner.name) ###some_pet.owner will return owner object
+
+
+####### Joins ############
+
+
+#### Left outer join
+# db.session.query()    ## this allows to set any query in slqalchemy
+# left_join = db.session.query(Person, Pet).outerjoin(Pet, Person.id == Pet.owner_id).all()
+# # print(left_join)
+# for i in left_join[:-1]:
+#     print(i[1].name)
+#
+
+#### right outer join
+left_join = db.session.query(Person, Pet).outerjoin(Pet, Person.id == Pet.owner_id).all()
+# print(left_join)
+for i in left_join[:-1]:
+    print(i[1].id,i[1].name, i[1].owner_id )
